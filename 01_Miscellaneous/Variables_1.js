28e7ac95b41da665665cb3a92dabc0131578052d
JensKristianRuben <Jenskristian@gmail.dk>
2025-08-28 10:59:02 +0200

    course notes added

---
+++ 01_Miscellaneous/Variables_1.js
@@ -0,0 +1,16 @@
+
+
+
+//use const if possible, othersie use let
+
+const personName = "Jens"
+
+
+console.log("Hello world!", personName);
+
+
+//String literals
+
+const firstgreeting = 'it is a nice day today" ';
+const secondgreeting = "it's is a nice day today";
+const thirdGreeting = `It's a nice " day today` // multi lining possible
