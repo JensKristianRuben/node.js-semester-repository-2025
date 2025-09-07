3f3e91b15beb9d34e24beb1833557644043c77b0
JensKristianRuben <Jenskristian@gmail.dk>
2025-09-07 15:14:18 +0200

    added more topics to node.js intro - also added sliding up animation when swapping subject

--- 02_Course_notes/Course_notes_app/index.js
+++ 02_Course_notes/Course_notes_app/index.js
@@ -1,7 +1,20 @@
 function copyCommand(button) {
     const command = button.previousElementSibling.innerText;
-     navigator.clipboard.writeText(command).then(() => {    
-      button.style.backgroundColor = "#339933";
-      setTimeout(() => (button.style.backgroundColor = "#f3f4f6"), 1500);
+    navigator.clipboard.writeText(command).then(() => {
+        button.style.backgroundColor = "#339933";
+        setTimeout(() => (button.style.backgroundColor = "#f3f4f6"), 1500);
     });
+}
+
+function navigateClassTopics(topic) {
+
+    const currentActive = document.querySelector('.subjects.active');
+    if (currentActive) {
+        currentActive.classList.remove('active');
+    }
+    const subject = document.querySelector(`[data-topic="${topic}"]`);
+    console.log(subject);
+
+    subject.classList.add('active');
+
 }
