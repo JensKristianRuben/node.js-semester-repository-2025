9b28170661d96fd0706ab69e28b2f19b8c14bec4
JensKristianRuben <Jenskristian@gmail.dk>
2025-09-18 10:47:47 +0200

    date logic added

--- 04._Date/app.js
+++ 04._Date/app.js
@@ -3,11 +3,30 @@ const express = require("express");
 const app = express();
 
 console.log(Date.now()); // Unix Epoch TIme Seconds Since Jan. 1st 1970
+console.log(new Date()); // UTC 
+
+console.log(new Date());
+
+
+// Opgave - returner den måned vi er i
+app.get("/months", (req, res) => {
+
+    const currentMonth = new Date().toLocaleDateString("default", { month: "long" });
+    res.send({ Data: currentMonth })
+});
+
+
+app.get("/day", (req, res) => {
+    const newCurrentDay = new Date().toLocaleDateString("default", { weekday: "long"})
+
+    res.send ({date: currentDay, newCurrentDay});
+})
+
 
 
 
 const PORT = 8080
 app.listen(PORT, () => {
     console.log("Server is running on PORT", PORT);
-    
+
 });
