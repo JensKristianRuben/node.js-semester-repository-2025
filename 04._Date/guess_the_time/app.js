5ca649f6a00eeb2cf4876743a3177ea065fa5fd4
JensKristianRuben <Jenskristian@gmail.dk>
2025-09-19 14:52:58 +0200

    clock visuals added

---
+++ 04._Date/guess_the_time/app.js
@@ -0,0 +1,25 @@
+const exprres = require("express");
+
+const app = exprres();
+
+
+app.get("/time", (req, res) => {
+
+  const maxDate = Date.now();
+  const randomTimeStamp = Math.floor(Math.random() * maxDate);
+  const timestamp = new Date(randomTimeStamp);
+  
+  console.log("The time is: ", timestamp.toLocaleTimeString());
+
+  res.send({ data: timestamp.toLocaleTimeString("da-DK", {hourCycle: "h12"})});
+});
+
+app.get("/", (req, res) => {
+  
+  res.sendFile(__dirname + "/static/index.html")
+})
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("The server is running on port:", PORT);
+});
