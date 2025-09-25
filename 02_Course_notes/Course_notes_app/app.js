e4fec159a57b6ea34f82007dba5c24420c248220
JensKristianRuben <Jenskristian@gmail.dk>
2025-09-25 21:20:18 +0200

    server added to project

---
+++ 02_Course_notes/Course_notes_app/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from "path"
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("public/index.html"))
+})
+
+
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log("Server is running on: ", PORT);
+    
+});
