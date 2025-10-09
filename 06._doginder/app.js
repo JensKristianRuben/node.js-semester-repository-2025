47416194a6460ffd84da808f28ce1f9ce690af89
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-09 10:15:27 +0200

    major changes to doginder

--- 06._doginder/app.js
+++ 06._doginder/app.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(express.static("public"))
 
+
+
+// ========================================== PAGES ==========================================
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("public/pages/frontend/index.html"))
 })
@@ -20,20 +23,16 @@ app.get("/matches", (req, res) => {
 
 // ========================================= API ===========================================
 
-app.get("/api/matches", (req, res) => {
-    
-    
-    
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response = response.json())
-    .then()
+import { getMatches } from './util/matchesUtil.js';
+import { da } from '@faker-js/faker';
 
-
-    res.send();
+app.get("/api/matches", async (req, res) => {
+    const matches = await getMatches();
+    res.send({ data: matches })
 })
 
-const PORT = process.env.PORT;
+
+const PORT = Number(process.env.PORT);
 app.listen(PORT, () => {
     console.log("Server is starting on port: ", PORT);
-    
 })
