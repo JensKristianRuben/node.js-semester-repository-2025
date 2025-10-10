97da370344ea28567f56cacee0a4ee4e39ae4aec
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-10 11:57:05 +0200

    added vercel.json file for deploy

--- 02_Course_notes/Course_notes_app/app.js
+++ 02_Course_notes/Course_notes_app/app.js
@@ -5,10 +5,42 @@ const app = express();
 
 app.use(express.static('public'));
 
+
+
+
+
+// ========================================API========================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========================================PAGES========================================
+
+
+
+
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("public/index.html"))
 })
 
+
+
+
+
+
+
+
+
+// ========================================CONFIG========================================
 const PORT = Number(process.env.PORT)
 app.listen(PORT, () => {
     console.log("Server is running on: ", PORT);
