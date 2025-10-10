8644ec311a96e4c2d987fb14f870a9669c243c20
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-10 13:36:28 +0200

    changes

--- 02_Course_notes/Course_notes_app/app.js
+++ 02_Course_notes/Course_notes_app/app.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import path from "path"
 import fs from 'fs'
-// import serverlessHttp from 'serverless-http';
 
 const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 
 
@@ -32,7 +31,7 @@ const indexPage = fs.readFileSync(path.resolve("./public/index.html"), "utf8")
 
 
 app.get("/", (req, res) => {
-    res.send("<h2>Can work with Git in the terminal</h2>")
+    res.send(indexPage)
 })
 
 
@@ -48,5 +47,3 @@ const PORT = Number(process.env.PORT)
 app.listen(PORT, () => {
     console.log("Server is running on: ", PORT);
 });
-
-// export default serverlessHttp(app);
