a7362b0ac6911fe089c7a50b1877f6c56a0d8daa
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-10 13:49:47 +0200

    changes

--- 02_Course_notes/Course_notes_app/app.js
+++ 02_Course_notes/Course_notes_app/app.js
@@ -1,49 +1,26 @@
-import express from 'express'
-import path from "path"
-import fs from 'fs'
+import express from "express";
+import path, { dirname } from "path";
+import fs from "fs";
 
 const app = express();
 
-// app.use(express.static('public'));
-
-
-
-
+app.use(express.static("public"));
 
 // ========================================API========================================
 
 app.get("/api/hello", (req, res) => {
-    res.send({data: "Hallo"})
-})
-
-
-
-
-
-
-
-
+  res.send({ data: "Hallo" });
+});
 
 // ========================================PAGES========================================
 
-
-const indexPage = fs.readFileSync(path.resolve("./public/index.html"), "utf8")
-
-
 app.get("/", (req, res) => {
-    res.send(indexPage)
-})
-
-
-
-
-
-
-
-
+  const indexPage = fs.readFileSync(path.resolve("./public/index.html"), "utf8");
+  res.sendFile(indexPage);
+});
 
 // ========================================CONFIG========================================
-const PORT = Number(process.env.PORT)
+const PORT = Number(process.env.PORT);
 app.listen(PORT, () => {
-    console.log("Server is running on: ", PORT);
+  console.log("Server is running on: ", PORT);
 });
