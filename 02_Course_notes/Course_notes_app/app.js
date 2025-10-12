6594c950ed3f2e0a5a178816d8c887029e3893e0
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-12 20:40:15 +0200

    checking aoo.use method

--- 02_Course_notes/Course_notes_app/app.js
+++ 02_Course_notes/Course_notes_app/app.js
@@ -10,7 +10,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url); //
 const __dirname = dirname(__filename); //
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 // ========================================API========================================
 
 // ========================================PAGES========================================
