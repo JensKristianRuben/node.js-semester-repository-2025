9e9f5f2b10597d96a4e3cd38d9ac9c6b81520963
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-23 10:01:13 +0200

    added stuff

--- 06._doginder/app.js
+++ 06._doginder/app.js
@@ -1,49 +1,22 @@
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static("public"));
 
-app.use(express.static("public"))
+app.use(express.urlencoded());
 
-
-
-// ========================================== PAGES ==========================================
-
-// import fs from 'fs'
-
-
-// //blokere når vi starter appen op - derfor vil vi gerne have den uden for selve endpointet
-// const frontpagePage = fs.readFileSync("./public/pages/frontend/index.html").toString();
-// const matchesPage = fs.readFileSync("./public/pages/matches/matches.html").toString();
-
-// const header = fs.readFileSync("./public/components/header/header.html").toString();
-// const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+import matchesRouter from './routers/matchesRouter.js';
+app.use(matchesRouter);
+import pagesRouter from './routers/pagesRouter.js';
+app.use(pagesRouter);
+import contactRouter from './routers/contactRouter.js';
+app.use(contactRouter);
 
 
-import { frontpagePage, matchesPage } from './util/pageUtil.js';
 
-app.get("/", (req, res) => {
-    res.send(header + frontpagePage + footer)
-})
-
-app.get("/matches", (req, res) => {
-    res.sendFile(header + matchesPage + footer)
-})
-
-
-// ========================================= API ===========================================
-
-import { getMatches } from './util/matchesUtil.js';
-import { da } from '@faker-js/faker';
-
-app.get("/api/matches", async (req, res) => {
-    const matches = await getMatches();
-    res.send({ data: matches })
-})
-
-
-const PORT = Number(process.env.PORT);
+const PORT = Number(process.env.PORT) || 8080;
 app.listen(PORT, () => {
-    console.log("Server is starting on port: ", PORT);
-})
+    console.log('Server is running on port:', PORT); 
+});
