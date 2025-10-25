1e0720ad42fa264788980393cb3c66b356ca2d2e
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-25 15:24:19 +0200

    added test server for sending emails

---
+++ 01_Miscellaneous/send_email_form/app.js
@@ -0,0 +1,37 @@
+import express, { json } from "express";
+import { Resend } from "resend";
+import dotenv from 'dotenv'
+
+
+const app = express();
+dotenv.config();
+
+app.use(express.json()); // so we can parse JSON
+
+
+
+app.post("/send", async (req, res) => {
+  const email = req.body.email;
+  console.log(email);
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  const { data, error } = await resend.emails.send({
+    from: "noreply <noreply@arbezzebra.dk>",
+    to: [email],
+    subject: "hello world",
+    html: "<p>it works!</p>",
+  });
+
+  
+  if (error) {    
+      return res.status(500).send({data: error})
+    } 
+    
+    res.send({ data: data });
+});
+
+
+
+const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on PORT: ", PORT));
